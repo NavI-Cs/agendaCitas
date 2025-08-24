05d31d149fd011f595ef374aa751a85216a1074e
NavI-Cs <rics2015ch@gmail.com>
2025-08-24 14:52:01 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'curious-facts',
+  webDir: 'dist'
+};
+
+export default config;
